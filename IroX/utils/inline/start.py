a87675f71a5a49afa38d65854089a02a75e0dbd2
Lucky Raja <108863687+abcrahulk@users.noreply.github.com>
2023-08-07 17:40:25 +0530

    Update start.py

--- IroX/utils/inline/start.py
+++ IroX/utils/inline/start.py
@@ -36,7 +36,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(text="♡ Hᴇʟᴘ & Cᴏᴍᴍᴀɴᴅꜱ ♡", callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(text="☠Sᴏᴜʀᴄᴇ☠", url=config.GITHUB_REPO),
+            InlineKeyboardButton(text="☠ Channel ☠", url=config.GITHUB_REPO),
             InlineKeyboardButton(text="ღ Oᴡɴᴇʀ ღ", user_id=OWNER),
         ],
     ]
